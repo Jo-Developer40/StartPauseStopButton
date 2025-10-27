a3f8295e7a61123703e111e5b8ec82b2b0d1cfbf
Jo-Developer40 <juergen.schulz@me.com>
2025-10-27 08:49:53 +0100

    Initial Commit

---
+++ Button2/Button2App.swift
@@ -0,0 +1,17 @@
+//
+//  Button2App.swift
+//  Button2
+//
+//  Created by Juergen on 27.10.25.
+//
+
+import SwiftUI
+
+@main
+struct Button2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
